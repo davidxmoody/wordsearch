4c2a03f97d074d74fccccf93e048ba7dad6ce8b0
David Moody <david@davidxmoody.com>
2014-11-23 10:59:19 +0000

    Very basic highscore demo

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var koa = require('koa');
 var serve = require('koa-static');
+var route = require('koa-route');
 var app = koa();
 
+var highscores = { 'Me': 1010 };
+
 app.use(serve('.'));
+
+app.use(route.get('/highscores', function *() {
+  this.body = JSON.stringify(highscores);
+}));
+
+app.use(route.get('/highscores/:score', function *(score) {
+  highscores['You'] = score;
+  this.body = "High score submitted, thank you.";
+}));
 
 app.listen(3000);
